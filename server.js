fe00705ff9d434604704826488c5846563c30af5
Dimitrije Krstic <dm868361@dal.ca>
2025-03-09 21:34:08 -0400

    Lab5

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const app = express();
 const PORT = 3000;
 
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname)));
 
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 
